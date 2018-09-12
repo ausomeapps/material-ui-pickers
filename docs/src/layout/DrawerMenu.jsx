ea5bd453d9ac8c733a1195f9e8073a0e2b485f6d
Syed Haris Ali <syedhali07@gmail.com>
2018-09-12 10:42:01 -0500

    Reconfiguring file directory for npm

--- docs/src/layout/DrawerMenu.jsx
+++ docs/src/layout/DrawerMenu.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { Divider, Toolbar, Typography, withStyles } from '@material-ui/core';
 
 import NavigationMenu from './NavigationMenu';
-import { version } from '../../../lib/package.json';
+import { version } from '../../../package.json';
 
 const DrawerMenu = ({ classes }) => (
   <div className={classes.drawerRoot}>
